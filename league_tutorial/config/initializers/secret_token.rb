0b295b8258330e3aa6737a2e9e1f6142ba9c618b
zrivest <zrivest@gmail.com>
2013-11-03 16:23:40 -0600

    first commit

---
+++ league_tutorial/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeagueTutorial::Application.config.secret_token = '12f490dbee4e1e0df1c2658e2d6b40fff8edbe257ffe99edc727c8e9e7e9faef03d3b39bbe961b2857e85d68c46408e9db16800698f5d2fbd3e633e88678ba06'
